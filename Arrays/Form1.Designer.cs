45b8e997c2fa4e80d729388d4bc23aa3fa7c2d77
Oklard <54605502+Oklard@users.noreply.github.com>
2022-04-22 15:56:18 -0300

    Add files via upload

---
+++ Arrays/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testeVetor = new System.Windows.Forms.Button();
+            this.btnVetorTexto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // testeVetor
+            // 
+            this.testeVetor.Location = new System.Drawing.Point(305, 108);
+            this.testeVetor.Name = "testeVetor";
+            this.testeVetor.Size = new System.Drawing.Size(135, 88);
+            this.testeVetor.TabIndex = 0;
+            this.testeVetor.Text = "Teste vetor";
+            this.testeVetor.UseVisualStyleBackColor = true;
+            this.testeVetor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnVetorTexto
+            // 
+            this.btnVetorTexto.Location = new System.Drawing.Point(305, 227);
+            this.btnVetorTexto.Name = "btnVetorTexto";
+            this.btnVetorTexto.Size = new System.Drawing.Size(135, 88);
+            this.btnVetorTexto.TabIndex = 1;
+            this.btnVetorTexto.Text = "Vetor texto";
+            this.btnVetorTexto.UseVisualStyleBackColor = true;
+            this.btnVetorTexto.Click += new System.EventHandler(this.btnVetorTexto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVetorTexto);
+            this.Controls.Add(this.testeVetor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testeVetor;
+        private System.Windows.Forms.Button btnVetorTexto;
+    }
+}
+
---
+++ Arrays/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] vetor = new int[4];
+            vetor[0] = 7;
+            vetor[1] = 3;
+            vetor[2] = 5;
+            vetor[3] = 1;
+            int valor = vetor[1];
+            System.Array.Sort(vetor);
+            for (int i = 0; i < 4; i++)
+                MessageBox.Show(vetor[i].ToString(), "array");
+
+        }
+
+        private void btnVetorTexto_Click(object sender, EventArgs e)
+        {
+            string[] semana = new string[7];
+            semana[0] = "Doomingo";
+            semana[1] = "segunda"; 
+            semana[2] = "terça"; 
+            semana[3] = "quarta";
+            semana[4] = "quinta";
+            semana[5] = "sexta";
+            semana[6] = "sabado";
+
+            string valor = semana[1];
+            for (int i = 0; i < 7; i++)
+                MessageBox.Show(semana[i].ToString(), "array");
+        }
+    }
+}
